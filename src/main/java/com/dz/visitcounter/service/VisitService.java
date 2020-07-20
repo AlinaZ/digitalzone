4f97f4842ca14672589c745c6b0225572c318c6e
Загитова Алина Ильгизовна <Zagitova.A.I@sberbank.ru>
2020-07-20 09:37:38 +0500

    added no such page_id handling

--- src/main/java/com/dz/visitcounter/service/VisitService.java
+++ src/main/java/com/dz/visitcounter/service/VisitService.java
@@ -1,7 +1,9 @@
 package com.dz.visitcounter.service;
 
+import com.dz.visitcounter.dao.PageDao;
 import com.dz.visitcounter.dao.UserDao;
 import com.dz.visitcounter.dao.VisitDao;
+import com.dz.visitcounter.model.Page;
 import com.dz.visitcounter.model.User;
 import com.dz.visitcounter.model.Visit;
 import com.dz.visitcounter.view.PeriodStats;
@@ -14,36 +16,43 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.NoResultException;
 import java.util.Date;
+import java.util.logging.Logger;
 
 @Service
 public class VisitService {
 
     private final VisitDao dao;
     private final UserDao userDao;
+    private final PageDao pageDao;
 
     @Autowired
-    public VisitService(VisitDao dao, UserDao userDao) {
+    public VisitService(VisitDao dao, UserDao userDao, PageDao pageDao) {
         this.dao = dao;
         this.userDao = userDao;
+        this.pageDao = pageDao;
     }
 
     @Transactional
     public SuccessView add(VisitView view, String ip) {
         Visit visit = new Visit();
+        SuccessView todayStats = new SuccessView();
         try {
             User user = userDao.loadByIp(ip);
         } catch (NoResultException ex) {
             User newUser = new User(ip);
             userDao.save(newUser);
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!");
-
         }
         visit.setUser_id(view.userid);
-        visit.setPage_id(view.pageid);
-        visit.setTime(new Date());
-        visit.setIp(ip);
-        dao.addVisit(visit);
-        SuccessView todayStats = new SuccessView();
+        Page page = pageDao.loadById(view.pageid);
+        if (page != null) {
+            visit.setPage_id(view.pageid);
+            visit.setTime(new Date());
+            visit.setIp(ip);
+            dao.addVisit(visit);
+        }
+        else {
+            todayStats.result="Error: в базе нет страницы с таким page_id";
+        }
         todayStats.todayVisits = dao.todayVisits();
         todayStats.uniqueVisitersToday = dao.uniqueTodayVisits();
         return todayStats;
